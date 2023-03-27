6d4a9fddbcb92663bbf3206325803b63e83824a3
tgthiag <tgthiag@gmail.com>
2023-03-27 03:59:32 -0300

    Implementing methods GET

---
+++ db.py
@@ -0,0 +1,26 @@
+Products = [
+    {
+        'id': 1,
+        'marca': 'Fiat',
+        'modelo': 'Marea',
+        'ano': 1999
+    },
+    {
+        'id': 2,
+        'marca': 'Chevrolet',
+        'modelo': 'Onix',
+        'ano': 2015
+    },
+    {
+        'id': 3,
+        'marca': 'Volkswagen',
+        'modelo': 'Gol',
+        'ano': 2010
+    },
+    {
+        'id': 4,
+        'marca': 'Toyota',
+        'modelo': 'Corolla',
+        'ano': 2022
+    }
+]
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from db import Products
 
 app = Flask(__name__)
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    return Products
 
 app.run()
