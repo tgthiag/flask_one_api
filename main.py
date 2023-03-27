624a57ebeeb8d08d901e3db7e5490a4d87172f66
tgthiag <tgthiag@gmail.com>
2023-03-27 04:14:50 -0300

    API Response & Jsonify

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from db import Products
 
 app = Flask(__name__)
 
 @app.route('/products', methods=['GET'])
 def get_products():
-    return Products
+    return make_response(
+        jsonify(Products)
+    )
 
 app.run()
