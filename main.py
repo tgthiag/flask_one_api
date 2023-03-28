1a639f03399f092c5eccfb8f8235968560fc5892
tgthiag <tgthiag@gmail.com>
2023-03-28 11:59:08 -0300

    Post method

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, request, jsonify
 from db import Products
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
 
 @app.route('/products', methods=['GET'])
 def get_products():
     return make_response(
         jsonify(Products)
     )
+
+
+@app.route('/products', methods=['POST'])
+def create_product():
+    item = request.json
+    Products.append(item)
+    response = make_response("Success")
+    response.status_code = 200
+    return response
+
 
 app.run()
