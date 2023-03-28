1b5e78680106ed72099f4906809325362421e6e9
tgthiag <tgthiag@gmail.com>
2023-03-28 12:10:47 -0300

    Auto Reload

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ from db import Products
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
-
+app.config.update(TEMPLATES_AUTO_RELOAD=True)
 
 @app.route('/products', methods=['GET'])
 def get_products():
     return make_response(
         jsonify(Products)
     )
-
 
 @app.route('/products', methods=['POST'])
 def create_product():
@@ -19,6 +18,5 @@ def create_product():
     response = make_response("Success")
     response.status_code = 200
     return response
-
 
 app.run()
